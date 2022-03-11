b375142685eb00b27f7fe82a8c322ac12d5e2ab5
Eugene Ivanov <wied11@yandex.ru>
2022-03-11 20:04:52 +0500

    initial commit

---
+++ app/src/main/java/com/evg_ivanoff/shoppinglist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.evg_ivanoff.shoppinglist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
