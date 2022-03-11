4b7804fcf9d508605c753eb5fb843d51b0160b2e
Eugene Ivanov <wied11@yandex.ru>
2022-03-11 21:02:42 +0500

    Changes: работа с data слоем

--- app/src/main/java/com/evg_ivanoff/shoppinglist/domain/GetShopItemUseCase.kt
+++ app/src/main/java/com/evg_ivanoff/shoppinglist/domain/GetShopItemUseCase.kt
@@ -2,7 +2,7 @@ package com.evg_ivanoff.shoppinglist.domain
 
 class GetShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
-    fun getShopItem(shopItemId: Int){
-        shopListRepository.getShopItem(shopItemId)
+    fun getShopItem(shopItemId: Int): ShopItem{
+        return shopListRepository.getShopItem(shopItemId)
     }
 }
--- app/src/main/java/com/evg_ivanoff/shoppinglist/domain/ShopListRepository.kt
+++ app/src/main/java/com/evg_ivanoff/shoppinglist/domain/ShopListRepository.kt
@@ -4,6 +4,6 @@ interface ShopListRepository {
     fun addShopItem(shopItem: ShopItem)
     fun deleteShopItem(shopItem: ShopItem)
     fun editShopItem(shopItem: ShopItem)
-    fun getShopItem(shopItemId: Int)
+    fun getShopItem(shopItemId: Int): ShopItem
     fun getShopList(): List<ShopItem>
 }
